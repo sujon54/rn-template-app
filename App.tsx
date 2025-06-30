07a40390932ed40438f2640008c56ffe081e5424
sujon54 <sazzad@textiletoday.com.bd>
2025-06-30 12:16:47 +0600

    added navigation

--- App.tsx
+++ App.tsx
@@ -5,64 +5,21 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View, Platform } from 'react-native';
-import React, { useContext, useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import SecondScreen from './src/screens/SecondScreen';
-
-const Stack = createNativeStackNavigator();
- 
- 
-const StackNav = () => {
-  const [isLogged, setIsLoged] = useState(true)
-
-  return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{ headerShown: false, animation: Platform.OS == "ios" ? "default" : "none" }}
-    >  
-      {/* {isLogged ? <Stack.Screen name="Splash" component={Splash} /> : null}
-
-      {
-        loggedUser?.id ? (
-          <> */}
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="SecondScreen" component={SecondScreen} />
-            {/* <Stack.Screen name="EventList" component={EventList} />
-            <Stack.Screen name="EventSchedules" component={EventSchedules} />
-            <Stack.Screen name="EventDetails" component={EventDetails} />
-            <Stack.Screen name="ScanQRTD" component={ScanQRTD} />
-            <Stack.Screen name="ScanQRTP" component={ScanQRTP} /> */}
-          {/* </>
-        )
-        : <Stack.Screen name="Login" component={Login} />
-      } */}
-    </Stack.Navigator>
-  );
-};
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'react-native';
+import AppNavigator from './src/navigation';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <NavigationContainer>
-        <StackNav />
-
-      </NavigationContainer>
-    // <View style={styles.container}>
-    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-    //   <NewAppScreen templateFileName="App.tsx" />
-    // </View>
+    <Provider store={store}>
+      <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
+      <AppNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
